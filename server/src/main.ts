c1d45eb1f164ed7dde526a2d92ade149dd3debc7
Jonathan Harrison <jonathan.harrison@whitelabel-loyalty.com>
2022-12-02 13:27:29 +0000

    Setting up mail server backend and API

---
+++ server/src/main.ts
@@ -0,0 +1,52 @@
+import path from 'path';
+import express, { Express, NextFunction, Request, Response } from 'express';
+import { serverInfo } from './ServerInfo';
+import * as IMAP from './IMAP';
+import * as SMTP from './SMTP';
+import * as Contacts from './Contacts';
+
+const app: Express = express();
+app.use(express.json());
+app.use("/", express.static(path.join(__dirname, "../../client/dist")));
+
+app.use(function(inRequest: Request, inResponse: Response, inNext: NextFunction) {
+	inResponse.header("Access-Control-Allow-Origin", "*");
+	inResponse.header("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+	inResponse.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	inNext();
+});
+
+app.get("/mailboxes", async (inRequest: Request, inResponse: Response) => {
+	try {
+		const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+		const mailboxes: IMAP.IMailbox[] = await imapWorker.listMailboxes();
+		inResponse.json(mailboxes);
+	} catch (inError) {
+		inResponse.send("error");
+	}
+});
+
+app.get("/mailboxes/:mailbox", async (inRequest: Request, inResponse: Response) => {
+	try {
+		const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+		const messages: IMAP.IMessage[] = await imapWorker.listMessages({
+			mailbox: inRequest.params.mailbox,
+		});
+		inResponse.json(messages);
+	} catch (inError) {
+		inResponse.send("error");
+	}
+});
+
+app.get("/messages/:mailbox/:id", async (inRequest: Request, inResponse: Response) => {
+	try {
+		const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+		const messageBody: string = await imapWorker.getMessageBody({
+			mailbox: inRequest.params.mailbox,
+			id: parseInt(inRequest.params.id, 10)
+		});
+		inResponse.send(messageBody);
+	} catch (inError) {
+		inResponse.send("error");
+	}
+});
